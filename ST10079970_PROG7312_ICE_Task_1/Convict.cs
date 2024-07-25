35d1fab4579d18571652703b19504ca6fffe8a35
Dillon-Duncan <145899893+Dillon-Duncan@users.noreply.github.com>
2024-07-25 19:23:49 +0200

    Add files via upload

---
+++ ST10079970_PROG7312_ICE_Task_1/Convict.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10079970_PROG7312_ICE_Task_1
+{
+    public class Convict
+    {
+        private int convictID, senetnce, cellNumber;
+        private string name, surname, crime;
+
+        public Convict(int convictID, int senetnce, int cellNumber, string name, string surname, string crime)
+        {
+            this.convictID = convictID;
+            this.senetnce = senetnce;
+            this.cellNumber = cellNumber;
+            this.name = name;
+            this.surname = surname;
+            this.crime = crime;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return this.convictID;
+
+                    case 1:
+                        return this.senetnce;
+
+                    case 2:
+                        return this.cellNumber;
+
+                    case 3:
+                        return this.name;
+
+                    case 4:
+                        return this.surname;
+
+                    case 5:
+                        return this.crime;
+
+                    default:
+                        return null;
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        this.convictID = (int)value;
+                        break;
+
+                    case 1:
+                        this.senetnce = (int)value;
+                        break;
+
+                    case 2:
+                        this.cellNumber = (int)value;
+                        break;
+
+                    case 3:
+                        this.name = (string)value;
+                        break;
+
+                    case 4:
+                        this.surname = (string)value;
+                        break;
+
+                    case 5:
+                        this.crime = (string)value;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public object this[string attrName]
+        {
+            get
+            {
+                switch (attrName.ToLower())
+                {
+                    case "convictid":
+                        return this.convictID;
+
+                    case "sentence":
+                        return this.senetnce;
+
+                    case "cellnumber":
+                        return this.cellNumber;
+
+                    case "name":
+                        return this.name;
+
+                    case "surname":
+                        return this.surname;
+
+                    case "crime":
+                        return this.crime;
+
+                    default:
+                        return null;
+                }
+            }
+            set
+            {
+                switch (attrName.ToLower())
+                {
+                    case "convictid":
+                        this.convictID = (int)value;
+                        break;
+
+                    case "sentence":
+                        this.senetnce = (int)value;
+                        break;
+
+                    case "cellnumber":
+                        this.cellNumber = (int)value;
+                        break;
+
+                    case "name":
+                        this.name = (string)value;
+                        break;
+
+                    case "surname":
+                        this.surname = (string)value;
+                        break;
+
+                    case "crime":
+                        this.crime = (string)value;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ST10079970_PROG7312_ICE_Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ST10079970_PROG7312_ICE_Task_1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Convict convict = new Convict(12345, 5, 101, "Swiper", "No Swiping", "Theft");
+
+            Console.WriteLine("Convict ID: " + convict[0]);
+            Console.WriteLine("Sentence: " + convict[1]);
+            Console.WriteLine("Cell Number: " + convict[2]);
+            convict[2] = 102;
+            Console.WriteLine("Name: " + convict[3]);
+            Console.WriteLine("Surname: " + convict[4]);
+            Console.WriteLine("Crime: " + convict[5]);
+
+            Console.WriteLine("\nUpdated Convict Information:");
+            Console.WriteLine("Convict ID: " + convict["convictid"]);
+            Console.WriteLine("Sentence: " + convict["sentence"]);
+            Console.WriteLine("Cell Number: " + convict["cellnumber"]);
+            Console.WriteLine("Name: " + convict["name"]);
+            Console.WriteLine("Surname: " + convict["surname"]);
+            Console.WriteLine("Crime: " + convict["crime"]);
+        }
+    }
+}
